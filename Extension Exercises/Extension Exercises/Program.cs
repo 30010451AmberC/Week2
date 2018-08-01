b016ecc653fa087c9f119d0ddb5a063a0ccaedba
Amber Cook <30010451@student.toiohomai.ac.nz>
2018-08-01 16:52:19 +1200

    extension exercise

---
+++ Extension Exercises/Extension Exercises/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.Write("Enter a number between 1 and 10");
+            int num1 =int.Parse(Console.ReadLine());
+
+            Console.Write("Enter another number between 1 and 10");
+            int num2 = int.Parse(Console.ReadLine());
+        }
+
+        public static void maths()
+        {
+            Console.Write("Would you like to 1)ADD 2)PRODUCT 3)DIVIDE these number?");
+            int keyPressed = int.Parse(Console.ReadKey());
+
+            if (keyPressed == 1)
+            {
+
+            }
+            else if (keyPressed == 2)
+            {
+            }
+            else if (keyPressed == 3)
+            {
+
+            }
+            else if (keyPressed != 1, 2, 3)
+            {
+
+            }
+
+        }
+    }
+}
+
