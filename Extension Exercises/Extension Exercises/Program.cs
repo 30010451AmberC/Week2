c5c465366841d42a3ee6b74d6bc4017e87644e08
Amber Cook <30010451@student.toiohomai.ac.nz>
2018-08-02 13:55:28 +1200

    Finished Extension Exercise

--- Extension Exercises/Extension Exercises/Program.cs
+++ Extension Exercises/Extension Exercises/Program.cs
@@ -10,34 +10,47 @@ namespace Extension_Exercises
     {
         static void Main(string[] args)
         {
+
+            
+            // INPUT get two numbers from user to use is equation.
           Console.Write("Enter a number between 1 and 10");
             int num1 =int.Parse(Console.ReadLine());
 
             Console.Write("Enter another number between 1 and 10");
             int num2 = int.Parse(Console.ReadLine());
+
+            maths(num1,num2);
         }
 
-        public static void maths()
+        public static void maths(int num1,int num2)
         {
-            Console.Write("Would you like to 1)ADD 2)PRODUCT 3)DIVIDE these number?");
-            int keyPressed = int.Parse(Console.ReadKey());
 
+            Console.Write("Press 1 to ADD, Press 2 for PRODUCT, Press 3 to DIVIDE these numbers");
+            int keyPressed = int.Parse(Console.ReadLine());
+
+            // IF ELSE MENU 
+            { 
             if (keyPressed == 1)
             {
-
+                int answer = num1 + num2;
             }
             else if (keyPressed == 2)
             {
+                int answer = num1 * num2;
             }
             else if (keyPressed == 3)
             {
-
+                int answer = num1 / num2;
             }
-            else if (keyPressed != 1, 2, 3)
+            else if (keyPressed != 1 || keyPressed != 2 || keyPressed != 3)
             {
-
+                Console.WriteLine("Press either 1, 2 or 3!");
             }
-
+            else
+            {
+                Console.WriteLine("You've broken the app");
+            }
+        }
         }
     }
 }
